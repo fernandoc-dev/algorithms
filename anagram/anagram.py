f24eec4e0e2436cd65d42b7466e55e3c7990ae5e
Fermnando Carrillo <fer@fernandoc.dev>
2023-03-18 09:34:31 -0500

    Determine if 2 given words are anagrams

---
+++ anagram/anagram.py
@@ -0,0 +1,54 @@
+"""
+Write a function that takes two words (Strings) and returns
+true or false (Bool) depending on whether they are anagrams or not.
+An Anagram is formed by rearranging ALL the letters of one initial word.
+It is NOT necessary to check that both words exist.
+Two exactly identical words are not an anagram.
+"""
+
+def anagram(word_1,word_2):
+    '''
+    Compare 2 words and determine if they are anagrams.
+
+    Parameters
+    ----------
+    word_1 : string
+    word_2 : string
+
+    Return
+    ------
+    anagram: boolean
+        The result of testing if string_1 and string_2 are anagrams
+
+    Example
+    --------
+    >>> anagram('calipso','ospilac')
+    True
+
+    >>> anagram('calipso','calipso')
+    False
+
+    >>> anagram('CalipSo','calipso')
+    False
+
+    >>> anagram('CalipSo','ospilac')
+    True
+    '''
+    word_1=word_1.lower()
+    word_2=word_2.lower()
+
+    if word_1.lower()==word_2.lower():
+        return False
+    word_1=[*word_1]
+    word_1.sort()
+    word_2=[*word_2]
+    word_2.sort()
+    if word_1==word_2:
+        return True
+    return False
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
+    print(anagram('CaliPso','ospilac'))
